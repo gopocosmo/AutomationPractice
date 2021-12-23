705c98229afa6b6e2009b5ffc144987aa72f0a5f
gopocosmo <vlad.domide@gmail.com>
2021-12-23 19:43:44 +0200

    ce vrei

--- src/test/java/Tests/RegisterTest.java
+++ src/test/java/Tests/RegisterTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -28,30 +29,52 @@ public class RegisterTest {
         WebElement skipSignInElement = driver.findElement(By.id("btn2"));
         skipSignInElement.click();
 
+        //verificam daca suntem pe pagina care trebuie
         //validare pagina de "Register"
         String actualRegister = driver.getTitle();
-        Assert.assertEquals("Regi2ster",actualRegister);
+        Assert.assertEquals("Register",actualRegister);
 
-
+        //first name
         WebElement firstNameElement = driver.findElement(By.cssSelector("input[placeholder='First Name']"));
         String firstNameElementvaloare = "Augustin";
         firstNameElement.sendKeys(firstNameElementvaloare);
 
+        //last name
+        WebElement lastNameElement = driver.findElement(By.cssSelector("input[placeholder='Last Name']"));
+        String lastNameElementvaloare = "Patron";
+        lastNameElement.sendKeys(lastNameElementvaloare);
 
+        //adresa
+        WebElement adresaElement = driver.findElement(By.cssSelector("textarea[ng-model='Adress']"));
+        String adresaElementValoare = "Cluj nr 23";
+        adresaElement.sendKeys(adresaElementValoare);
+
+        //incarcam o poza
+        WebElement imageElement = driver.findElement(By.id("imagesrc"));
+        imageElement.sendKeys("C:\\Users\\Gopo\\Pictures\\1.jpg");
+
+
+        /*
+
+        //email
+        WebElement emailElement = driver.findElement(By.cssSelector("input[type='email']"));
+        String emailElementValoare = "ceva@ceva.com";
+        emailElement.sendKeys(emailElementValoare);
+
+        //telefon
+        WebElement telElement = driver.findElement(By.cssSelector("input[type='tel']"));
+        String telElementValoare = "0264212666";
+        telElement.sendKeys(telElementValoare);
+
+        //gender
         WebElement genderElement = driver.findElement(By.cssSelector("input[value='Male']"));
         genderElement.click();
 
+        //hobby
         WebElement hobbyElement = driver.findElement(By.id("checkbox3"));
         hobbyElement.click();
 
-        WebElement skillsElement = driver.findElement(By.id("Skills"));
-        Select skillSelect = new Select(skillsElement);
-        skillSelect.selectByVisibleText("Android");
-
-        WebElement yearElement = driver.findElement(By.id("yearbox"));
-        Select yearSelect = new Select(yearElement);
-        yearSelect.selectByValue("1970");
-
+        //language
         WebElement languageElement = driver.findElement(By.id("msdd"));
         languageElement.click();
 
@@ -62,18 +85,66 @@ public class RegisterTest {
             }
         }
 
+        //clic altundeva pe pagina pt a inchide dropdownul de la language
         genderElement.click();
 
-        //adresa
-        WebElement adresaElement = driver.findElement(By.cssSelector("textarea[ng-model='Adress']"));
-        String adresaElementValoare = "Cluj nr 23";
-        adresaElement.sendKeys(adresaElementValoare);
+        //skills
+        WebElement skillsElement = driver.findElement(By.id("Skills"));
+        Select skillSelect = new Select(skillsElement);
+        skillSelect.selectByVisibleText("Android");
+
+
+
+        //country
+        WebElement countryElement = driver.findElement(By.cssSelector("span[aria-labelledby='select2-country-container']"));
+        countryElement.click();
+
+        //parcurgem lista de tari si selectam tara dorita
+        List<WebElement> countryElements = driver.findElements(By.xpath("//*[@id='country']/option"));
+        for (Integer i = 0;i < countryElements.size();i++){
+            if (countryElements.get(i).getText().equals("India")){
+                countryElements.get(i).click();
+            }
+        }
+
+        //anul
+        WebElement yearElement = driver.findElement(By.id("yearbox"));
+        Select yearSelect = new Select(yearElement);
+        yearSelect.selectByValue("1970");
+
+        //luna
+        WebElement monthElement = driver.findElement(By.cssSelector("select[placeholder='Month']"));
+        Select monthSelect = new Select(monthElement);
+        monthSelect.selectByValue("May");
+
+        //ziua
+        WebElement dayElement = driver.findElement(By.id("daybox"));
+        Select daySelect = new Select(dayElement);
+        daySelect.selectByValue("22");
 
         //parola
         WebElement parolaElement = driver.findElement(By.id("firstpassword"));
         String parolaElementValoare = "parola123";
         parolaElement.sendKeys(parolaElementValoare);
 
+        //confirmare parola
+        WebElement confirmareParolaElement = driver.findElement(By.id("secondpassword"));
+        String confirmareParolaElementValoare = "parola123";
+        confirmareParolaElement.sendKeys(confirmareParolaElementValoare);
+
+        //click "submit"
+        WebElement submitElement = driver.findElement(By.id("submitbtn"));
+        submitElement.click();
+
+
+
+        //comparam url-ul curent cu url-ul paginii de register, daca nu s-a schimbat nu a trecut testul
+        String actualRegisterDoi = driver.getTitle();
+        String urlPagina = "http://demo.automationtesting.in/Register.html";
+        Assert.assertEquals("Testul NU a functionat",actualRegisterDoi,urlPagina);
+
+
+         */
 
 
     }
